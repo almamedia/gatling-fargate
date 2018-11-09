80b955a3d7adfb2d38ecf2e14e50f0dbc7c6e759
Markus Heikkilä <markus.heikkila@almamedia.fi>
2018-11-09 09:52:57 +0200

    sync compiled simulations to S3 to speed up task start (only need to compile changed simulations on the first run)

--- docker/run-gatling.sh
+++ docker/run-gatling.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Sync config simulations and previous results (in case we want to combine results from previous runs) from s3
+# Sync config, simulations and previous results (in case we want to combine results from previous runs) from s3
 
 aws s3 sync s3://$WORK_BUCKET/user-files /opt/gatling/user-files
 aws s3 sync s3://$WORK_BUCKET/conf /opt/gatling/conf
 aws s3 sync s3://$WORK_BUCKET/results /opt/gatling/results
 
-export JAVA_OPTS="-Dsun.net.inetaddr.ttl=10"
-
 # Run gatling
-/opt/gatling/bin/gatling.sh $@
+JAVA_OPTS="-Dsun.net.inetaddr.ttl=10" /opt/gatling/bin/gatling.sh -bf /opt/gatling/user-files/bin $@
 
 # Sync report and logs to S3
 aws s3 sync /opt/gatling/results s3://$WORK_BUCKET/results
+# Sync compiled simulations to S3
+aws s3 sync /opt/gatling/user-files s3://$WORK_BUCKET/user-files
